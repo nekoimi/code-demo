b4c1427c2e6c129bb728dbd3ee522ea7adbb33ec
nekoimi <nekoimime@gmail.com>
2025-07-19 23:00:10 +0800

    up: rust

---
+++ rust/code2/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    // 常量
+    const A: i32 = 100;
+    const B: i32 = 200;
+    const C: i32 = 300;
+    const D: i32 = 400;
+    println!("A={}, B={}, C={}, D={}", A, B, C, D);
+}

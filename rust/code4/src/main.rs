1e9b666eb399a02e8f75b1d5d13d1cc7f3a0877c
nekoimi <nekoimime@gmail.com>
2025-07-21 19:27:14 +0800

    up

--- rust/code4/src/main.rs
+++ rust/code4/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
+
+
+    let v = {
+        let a = 1;
+        let b = 2;
+        a + b
+    };
+    println!("v = {}", v);
+
+    let add_func = add_2;
+    let result = add_func(1);
+    println!("result is {}", result);
+}
+
+
+fn add_2(a: i32) -> i32 {
+    if a < 10 {
+        return a + add_2(a + 1);
+    }
+
+    a + 2
 }
